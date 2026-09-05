abfefd532dca4b7386c362efc28e956735df49cb
AlexandrMaslov <alexandrmaslov2006@gmail.com>
2026-09-05 16:27:48 +0300

    Добавьте файлы проекта.

---
+++ DZ1mas/Accessory.c
@@ -0,0 +1,10 @@
+#include <locale.h>
+#include <stdio.h>
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	printf("          __   __   __   __ \n");
+	printf("  /|  /| |  | |__   __| |__  \n");
+	printf("   |   | |__|  __| |__  |__|    \n");
+	return 0;
+}
